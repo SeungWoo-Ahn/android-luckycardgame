fb6cc34f3455f7d65bc660bd10c0e96145c51aa9
SeungWoo-Ahn <rio970319@gmail.com>
2023-07-12 11:12:27 +0900

    Fix : 생성자, 리스트 타입, xml 빈 줄 수정

--- app/src/main/java/io/softeer/luckycardgame/player/Player.kt
+++ app/src/main/java/io/softeer/luckycardgame/player/Player.kt
@@ -3,13 +3,13 @@ package io.softeer.luckycardgame.player
 import io.softeer.luckycardgame.adapter.CardAdapter
 import io.softeer.luckycardgame.card.Card
 
-class Player constructor(
-    private val cardList: MutableList<Card>,
+class Player(
+    private val cardList: List<Card>,
     private var isMine : Boolean
 ) {
     fun changeToMyCard() {
         isMine = true
     }
-    fun adapterByPlayer() : CardAdapter = CardAdapter(cardList, isMine)
+    fun adapterByPlayer() : CardAdapter = CardAdapter(cardList.toMutableList(), isMine)
 
 }
