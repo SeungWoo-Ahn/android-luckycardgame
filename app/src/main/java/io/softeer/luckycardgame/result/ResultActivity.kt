48761a341fc20e602e6f957634359f6d4b886b0e
SeungWoo-Ahn <rio970319@gmail.com>
2023-07-13 16:26:29 +0900

    Feat : 결과화면 만들기

---
+++ app/src/main/java/io/softeer/luckycardgame/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package io.softeer.luckycardgame.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.softeer.luckycardgame.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
